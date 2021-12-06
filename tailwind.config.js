954301f638d2f865c2502156d3cd13269eacdb08
Johan <johanrivera172@gmail.com>
2021-12-06 17:47:49 -0500

    Adding backgrounds, buttons, video

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,15 @@ module.exports = {
     extend: {
       backgroundImage: theme => ({
         'wave-pattern': 'url(../images/wave-white.png)',
-        'wave-pattern-dots': 'url(../images/pattern-white-dots.png)'
-      })
+        'wave-pattern-dots': 'url(../images/pattern-white-dots.png)',
+        'pattern-characters-red': 'url(../images/pattern-characters-red.png)',
+        'pattern-character-mario': 'url(../images/character-l.png)',
+        'wave-pink': 'url(../images/wave-pink.png)',
+        'yellow-dots': 'url(../images/pattern-yellow-dots.png)',
+      }),
+      screens: {
+        'xsm': '320px'
+      }
     },
   },
   variants: {
